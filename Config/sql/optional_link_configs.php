399d60c6d907eeb0e5ac79916f2252b47717c240
arata <contact@materializing.net>
2013-12-30 05:58:35 +0900

    change CakeSchemaファイルを作成

--- Config/sql/optional_link_configs.php
+++ Config/sql/optional_link_configs.php
@@ -1,27 +1,27 @@
 <?php 
-/* SVN FILE: $Id$ */
-/* OptionalLinkConfigs schema generated on: 2013-11-24 01:11:43 : 1385223523*/
 class OptionalLinkConfigsSchema extends CakeSchema {
-	var $name = 'OptionalLinkConfigs';
 
-	var $file = 'optional_link_configs.php';
+	public $file = 'optional_link_configs.php';
 
-	var $connection = 'plugin';
+	public $connection = 'plugin';
 
-	function before($event = array()) {
+	public function before($event = array()) {
 		return true;
 	}
 
-	function after($event = array()) {
+	public function after($event = array()) {
 	}
 
-	var $optional_link_configs = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 8, 'key' => 'primary'),
-		'blog_content_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 1),
-		'status' => array('type' => 'boolean', 'null' => true, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	public $optional_link_configs = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 8, 'key' => 'primary'),
+		'blog_content_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 1),
+		'status' => array('type' => 'boolean', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
+
 }
-?>
