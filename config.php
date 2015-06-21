396d70bdfb7a12e234692176ec2bc429f7163e9d
arata <contact@materializing.net>
2015-06-21 23:09:48 +0900

    clear README改訂

--- config.php
+++ config.php
@@ -11,4 +11,4 @@ $description = 'ブログ記事に任意のURLを設定できます。';
 $author = 'arata';
 $url = 'http://www.materializing.net/';
 $adminLink = array('plugin' => 'optional_link', 'controller' => 'optional_link_configs', 'action' => 'index');
-$installMessage = 'ブログ記事の投稿画面に入力欄が追加されます。';
+$installMessage = 'ブログコンテンツ別に設定を追加し、有効化するとブログ記事の投稿画面に入力欄が追加されます。';
