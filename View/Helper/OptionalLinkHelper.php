27d22349284e8c6074108b3da7ef313c551cfb86
arata <contact@materializing.net>
2016-08-07 04:04:31 +0900

    Clean 整形

--- View/Helper/OptionalLinkHelper.php
+++ View/Helper/OptionalLinkHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * [Helper] Optional_link
  *
@@ -6,110 +7,117 @@
  * @author			arata
  * @license			MIT
  */
-class OptionalLinkHelper extends AppHelper {
-/**
- * ヘルパー
- *
- * @var array
- */
+class OptionalLinkHelper extends AppHelper
+{
+
+	/**
+	 * ヘルパー
+	 *
+	 * @var array
+	 */
 	public $helpers = array('BcBaser', 'Blog', 'BcUpload');
-	
-/**
- * アップロードファイルの保存URL
- * 
- * @var string
- */
+
+	/**
+	 * アップロードファイルの保存URL
+	 * 
+	 * @var string
+	 */
 	public $savedUrl = '';
-	
-/**
- * アップロードファイルの保存パス
- * 
- * @var string
- */
+
+	/**
+	 * アップロードファイルの保存パス
+	 * 
+	 * @var string
+	 */
 	public $savePath = '';
-	
-/**
- * constructer
- * 
- * @param View $View
- * @param array $settings
- */
-	public function __construct(View $View, $settings = array()) {
+
+	/**
+	 * constructer
+	 * 
+	 * @param View $View
+	 * @param array $settings
+	 */
+	public function __construct(View $View, $settings = array())
+	{
 		parent::__construct($View, $settings);
-		$this->savedUrl = baseUrl() .'files'. DS .'optionallink'. DS;
-		$this->savePath = OptionalLinkUtil::getSavePath() .DS;
+		$this->savedUrl	 = baseUrl() . 'files' . DS . 'optionallink' . DS;
+		$this->savePath	 = OptionalLinkUtil::getSavePath() . DS;
 	}
-	
-/**
- * 公開状態を取得する
- * 
- * @param array $data
- * @return boolean 公開状態
- */
-	public function allowPublish($data){
-		if (isset($data['OptionalLink'])){
+
+	/**
+	 * 公開状態を取得する
+	 * 
+	 * @param array $data
+	 * @return boolean 公開状態
+	 */
+	public function allowPublish($data)
+	{
+		if (isset($data['OptionalLink'])) {
 			$data = $data['OptionalLink'];
 		} elseif (isset($data['OptionalLinkConfig'])) {
 			$data = $data['OptionalLinkConfig'];
 		}
-		$allowPublish = (int)$data['status'];
+		$allowPublish = (int) $data['status'];
 		return $allowPublish;
 	}
-	
-/**
- * ファイルの公開状態を取得する
- *
- * @param array リンク状態のデータ
- * @return boolean 公開状態
- */
-	public function allowPublishFile($data) {
+
+	/**
+	 * ファイルの公開状態を取得する
+	 *
+	 * @param array リンク状態のデータ
+	 * @return boolean 公開状態
+	 */
+	public function allowPublishFile($data)
+	{
 		if (isset($data['OptionalLink'])) {
 			$data = $data['OptionalLink'];
 		}
 		$allowPublish = true;
 		// 期限を設定している場合に条件に該当しない場合は強制的に非公開とする
 		if (($data['publish_begin'] != 0 && $data['publish_begin'] >= date('Y-m-d H:i:s')) ||
-			($data['publish_end'] != 0 && $data['publish_end'] <= date('Y-m-d H:i:s'))) {
+				($data['publish_end'] != 0 && $data['publish_end'] <= date('Y-m-d H:i:s'))) {
 			$allowPublish = false;
 		}
 		return $allowPublish;
 	}
-	
-/**
- * オプショナルリンクの有効を判定する
- * 
- * @param array $post
- * @return boolean
- */	
-	public function isStatus($post = array()) {
-		if(!empty($post['OptionalLinkConfig']['status'])) {
-			if($post['OptionalLinkConfig']['status']) {
+
+	/**
+	 * オプショナルリンクの有効を判定する
+	 * 
+	 * @param array $post
+	 * @return boolean
+	 */
+	public function isStatus($post = array())
+	{
+		if (!empty($post['OptionalLinkConfig']['status'])) {
+			if ($post['OptionalLinkConfig']['status']) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
-/**
- * リンク文字列をチェックして判定する
- * 
- * @param array $post
- * @return string
- */
-	public function judgeLinkKinds($post = array()) {
+
+	/**
+	 * リンク文字列をチェックして判定する
+	 * 
+	 * @param array $post
+	 * @return string
+	 */
+	public function judgeLinkKinds($post = array())
+	{
 		$str = '';
-		
+
 		if ($post['OptionalLink']['status']) {
 			switch ($post['OptionalLink']['status']) {
 				case '1':
-					if(!empty($post['OptionalLink']['name'])) {
+					if (!empty($post['OptionalLink']['name'])) {
 						if ($post['OptionalLink']['blank']) {
 							$str = 'external';
 						}
 						$content = trim(strip_tags($post['OptionalLink']['name']));
 						// URLを分解する
-						$links = parse_url($content);
-						$path = pathinfo($content);
+						$links	 = parse_url($content);
+						$path	 = pathinfo($content);
 
 						if (!empty($path['extension'])) {
 							$str = OptionalLinkUtil::getUrlExtension($path['extension']);
@@ -127,11 +135,11 @@ class OptionalLinkHelper extends AppHelper {
 					break;
 
 				case '2':
-					if(!empty($post['OptionalLink']['file'])) {
+					if (!empty($post['OptionalLink']['file'])) {
 						$content = trim(strip_tags($post['OptionalLink']['file']));
 						// URLを分解する
-						$links = parse_url($content);
-						$path = pathinfo($content);
+						$links	 = parse_url($content);
+						$path	 = pathinfo($content);
 
 						if (!empty($path['extension'])) {
 							$str = OptionalLinkUtil::getUrlExtension($path['extension']);
@@ -141,34 +149,35 @@ class OptionalLinkHelper extends AppHelper {
 						}
 					}
 					break;
-				
+
 				default:
 					break;
 			}
 		}
 		return $str;
 	}
-	
-/**
- * オプショナルリンクの設定を反映したブログ記事リンクを出力する
- * - posts ビューで記事を取得する箇所で利用できる
- * - 利用方法：$this->OptionalLink->getPostTitle($post)
- * 
- * @param array $post
- * @param array $options
- * @return string
- */
-	public function getPostTitle($post = array(), $options = array()) {
-		$_options = array(
+
+	/**
+	 * オプショナルリンクの設定を反映したブログ記事リンクを出力する
+	 * - posts ビューで記事を取得する箇所で利用できる
+	 * - 利用方法：$this->OptionalLink->getPostTitle($post)
+	 * 
+	 * @param array $post
+	 * @param array $options
+	 * @return string
+	 */
+	public function getPostTitle($post = array(), $options = array())
+	{
+		$_options	 = array(
 			'link' => true,
 		);
-		$options = Hash::merge($_options, $options);
-		$url = '';
+		$options	 = Hash::merge($_options, $options);
+		$url		 = '';
 		$this->Blog->setContent($post['BlogPost']['blog_content_id']);
-		
+
 		if ($options['link']) {
 			if (isset($post['OptionalLink']) && $post['OptionalLink']['status'] >= 1) {
-				
+
 				switch ($post['OptionalLink']['status']) {
 					case '1':
 						$url = topLevelUrl(false) . $post['OptionalLink']['name'];
@@ -179,63 +188,65 @@ class OptionalLinkHelper extends AppHelper {
 							return $post['BlogPost']['name'];
 						}
 						break;
-					
+
 					case '2':
 						// サムネイル側へのリンクになるため、imgsize => large を指定する
-						$fileLink = $this->BcUpload->uploadImage('OptionalLink.file', $post['OptionalLink']['file'], array('imgsize' => 'large'));
-						$result = preg_match('/.+<?\shref=[\'|"](.*?)[\'|"].*/', $fileLink, $match);
+						$fileLink	 = $this->BcUpload->uploadImage('OptionalLink.file', $post['OptionalLink']['file'], array('imgsize' => 'large'));
+						$result		 = preg_match('/.+<?\shref=[\'|"](.*?)[\'|"].*/', $fileLink, $match);
 						if ($result) {
-							$post['OptionalLink']['name'] = $match[1];
-							$url = $post['OptionalLink']['name'];
-							$options['target'] = '_blank';
+							$post['OptionalLink']['name']	 = $match[1];
+							$url							 = $post['OptionalLink']['name'];
+							$options['target']				 = '_blank';
 						}
 						break;
-					
+
 					default:
 						break;
 				}
 			} else {
 				$url = array('admin' => false, 'plugin' => '', 'controller' => $this->Blog->blogContent['name'], 'action' => 'archives', $post['BlogPost']['no']);
 			}
-			
+
 			return $this->BcBaser->getLink($post['BlogPost']['name'], $url, $options);
 		} else {
 			return $post['BlogPost']['name'];
 		}
 	}
-	
-/**
- * ファイルが保存されているURLを取得する
- *
- * @param string $fileName
- * @return string
- */
-	public function getFileUrl ($fileName) {
+
+	/**
+	 * ファイルが保存されているURLを取得する
+	 *
+	 * @param string $fileName
+	 * @return string
+	 */
+	public function getFileUrl($fileName)
+	{
 		if ($fileName) {
 			return $this->savedUrl . $fileName;
 		} else {
 			return '';
 		}
 	}
-	
-/**
- * ファイルリンクタグを出力する
- * 
- * @param array $uploaderFile
- * @param array $options
- * @return string リンクタグ
- */
-	public function file ($uploaderFile, $options = array()) {
-		$_options = array(
-			'alt' => $uploaderFile['file'],
+
+	/**
+	 * ファイルリンクタグを出力する
+	 * 
+	 * @param array $uploaderFile
+	 * @param array $options
+	 * @return string リンクタグ
+	 */
+	public function file($uploaderFile, $options = array())
+	{
+		$_options	 = array(
+			'alt'	 => $uploaderFile['file'],
 			'target' => '_blank',
 		);
-		$options = Hash::merge($_options, $options);
-		
+		$options	 = Hash::merge($_options, $options);
+
 		if (isset($uploaderFile['OptionalLink'])) {
 			$uploaderFile = $uploaderFile['OptionalLink'];
 		}
-		
+
 		if (!empty($uploaderFile['file'])) {
 			$imgUrl = $this->getFileUrl($uploaderFile['file']);
 			//$pathInfo = pathinfo($uploaderFile['file']);
@@ -252,77 +263,74 @@ class OptionalLinkHelper extends AppHelper {
 		return '';
 	}
 
-
-/**
- * $postからURLを返す
- * 
- * @param array $post
- * @return string リンクURL
- */
-
-	public function getPostUrl($post) {
-
-		$url = $this->Blog->getPostLinkUrl($post);
-		$target = '';
+	/**
+	 * $postからURLを返す
+	 * 
+	 * @param array $post
+	 * @return string リンクURL
+	 */
+	public function getPostUrl($post)
+	{
+		$url	 = $this->Blog->getPostLinkUrl($post);
+		$target	 = '';
 		if ($post['OptionalLink']['status']) {
-		    if ($post['OptionalLink']['status'] == "1") {
-		        if ($post['OptionalLink']['nolink']) {
-		            $url = null;
-		        } else {
-		            if ($post['OptionalLink']['blank']) $target = 'target="_blank"';
-		            if ($post['OptionalLink']['name']) $url = $post['OptionalLink']['name'];
-		        }
-
-		    } elseif ($post['OptionalLink']['status'] == "2") {
-		        if ((!$post['OptionalLink']['publish_begin'] || strtotime($post['OptionalLink']['publish_begin']) < time()) &&
-		            (!$post['OptionalLink']['publish_end'] || strtotime($post['OptionalLink']['publish_end']) > time())
-		            ) {
-		            $target = 'target="_blank"';
-		            $url = "/files/optionallink/" . $post['OptionalLink']['file'];
-		        } else {
-		            $url = null;
-		        }
-		    }
+			if ($post['OptionalLink']['status'] == "1") {
+				if ($post['OptionalLink']['nolink']) {
+					$url = null;
+				} else {
+					if ($post['OptionalLink']['blank'])
+						$target	 = 'target="_blank"';
+					if ($post['OptionalLink']['name'])
+						$url	 = $post['OptionalLink']['name'];
+				}
+			} elseif ($post['OptionalLink']['status'] == "2") {
+				if ((!$post['OptionalLink']['publish_begin'] || strtotime($post['OptionalLink']['publish_begin']) < time()) &&
+						(!$post['OptionalLink']['publish_end'] || strtotime($post['OptionalLink']['publish_end']) > time())
+				) {
+					$target	 = 'target="_blank"';
+					$url	 = "/files/optionallink/" . $post['OptionalLink']['file'];
+				} else {
+					$url = null;
+				}
+			}
 		}
 
 		return $url;
 	}
 
-/**
- * $postからリンクターゲットの文字列を返す
- * 
- * @param array $post
- * @return string ターゲット(target="_blank")
- */
-
-	public function getPostTarget($post) {
-
-		$url = $this->Blog->getPostLinkUrl($post);
-		$target = '';
+	/**
+	 * $postからリンクターゲットの文字列を返す
+	 * 
+	 * @param array $post
+	 * @return string ターゲット(target="_blank")
+	 */
+	public function getPostTarget($post)
+	{
+		$url	 = $this->Blog->getPostLinkUrl($post);
+		$target	 = '';
 		if ($post['OptionalLink']['status']) {
-		    if ($post['OptionalLink']['status'] == "1") {
-		        if ($post['OptionalLink']['nolink']) {
-		            $url = null;
-		        } else {
-		            if ($post['OptionalLink']['blank']) $target = 'target="_blank"';
-		            if ($post['OptionalLink']['name']) $url = $post['OptionalLink']['name'];
-		        }
-
-		    } elseif ($post['OptionalLink']['status'] == "2") {
-		        if ((!$post['OptionalLink']['publish_begin'] || strtotime($post['OptionalLink']['publish_begin']) < time()) &&
-		            (!$post['OptionalLink']['publish_end'] || strtotime($post['OptionalLink']['publish_end']) > time())
-		            ) {
-		            $target = 'target="_blank"';
-		            $url = "/files/optionallink/" . $post['OptionalLink']['file'];
-		        } else {
-		            $url = null;
-		        }
-		    }
+			if ($post['OptionalLink']['status'] == "1") {
+				if ($post['OptionalLink']['nolink']) {
+					$url = null;
+				} else {
+					if ($post['OptionalLink']['blank'])
+						$target	 = 'target="_blank"';
+					if ($post['OptionalLink']['name'])
+						$url	 = $post['OptionalLink']['name'];
+				}
+			} elseif ($post['OptionalLink']['status'] == "2") {
+				if ((!$post['OptionalLink']['publish_begin'] || strtotime($post['OptionalLink']['publish_begin']) < time()) &&
+						(!$post['OptionalLink']['publish_end'] || strtotime($post['OptionalLink']['publish_end']) > time())
+				) {
+					$target	 = 'target="_blank"';
+					$url	 = "/files/optionallink/" . $post['OptionalLink']['file'];
+				} else {
+					$url = null;
+				}
+			}
 		}
 
 		return $target;
 	}
-
-
 
 }
